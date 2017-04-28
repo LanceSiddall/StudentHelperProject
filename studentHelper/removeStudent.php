7208b4a516f248474bd1f139c577a989d73476c6
AGrant324 <AGrant324@users.noreply.github.com>
2017-04-28 19:09:21 +0100

    Update removeStudent.php

--- studentHelper/removeStudent.php
+++ studentHelper/removeStudent.php
@@ -1,6 +1,6 @@
 <?php
-session_start();
-if(!$_SESSION['loggedIn']) {
+session_start();//Starts a php session so that if one hasn't already been started, there isn't an issue caused by calling a session variable
+if(!$_SESSION['loggedIn']) {//If the user attempts to access this page without being logged in, they will be redirected to the login page
 	header('location: index.php');
 }
 ?>
@@ -14,8 +14,7 @@ if(!$_SESSION['loggedIn']) {
 	</script>
 </head>
 <body>
-	<section id="externalWrapper">
-		<h2 style="display:none;">.</h2>
+	<section id="externalWrapper"><!--Container for everything other than the footer-->
 		<nav>
 			<ul>
 				<li>
@@ -25,7 +24,7 @@ if(!$_SESSION['loggedIn']) {
 					<a href="addRoom.php">Add Room</a>
 				</li>
 				<li>
-					<a href="removeStudent.php">Remove Student</a>
+					<a href="removeStudent.php" class="active">Remove Student</a><!--The anchor with the class of active will be white rather than grey to signify that it is the current page-->
 				</li>
 				<li>
 					<a href="courseworkDropoff.php">Coursework Dropoff</a>
@@ -35,26 +34,26 @@ if(!$_SESSION['loggedIn']) {
 				</li>
 			</ul>
 		</nav>
-		<section id="internalWrapper">
-				<div class="row cf">
-					<div class="half">
+		<section id="internalWrapper"><!--Container for the main content of the page-->
+				<div class="row cf"><!--Allows for the items to be aligned in a row-->
+					<div class="half"><!--Gives the containing items half the width of the parent to fill, expands when the page dimensions hit a breakpoint to 100%-->
 						<form name="display" action="lectRemoveStudent.php" method="POST" >
-							<div class="row cf">
-								<div class="half">
-									<div class="row cf">
+							<div class="row cf"><!--Allows for the items to be aligned in a row-->
+								<div class="half"><!--Gives the containing items half the width of the parent to fill, expands when the page dimensions hit a breakpoint to 100%-->
+									<div class="row cf"><!--Allows for the items to be aligned in a row-->
 											<h2>Remove Student</h>
 									</div>
 								</div>
 							</div>
-							<div class="row cf">
-								<div class="half">
+							<div class="row cf"><!--Allows for the items to be aligned in a row-->
+								<div class="half"><!--Gives the containing items half the width of the parent to fill, expands when the page dimensions hit a breakpoint to 100%-->
 									<p class="inputName" name="StudentEmail">Student Email: </p>
 								</div>
-								<div class="half">
+								<div class="half"><!--Gives the containing items half the width of the parent to fill, expands when the page dimensions hit a breakpoint to 100%-->
 									<input name="StudentEmail" required="true" type="text">
 								</div>
 							</div>
-							<div class="row cf">
+							<div class="row cf"><!--Allows for the items to be aligned in a row-->
 								<div class="full">
 									<input type="submit" id="submit" class="button" value="Submit" onclick="removeStudent()">
 								</div>
@@ -63,15 +62,14 @@ if(!$_SESSION['loggedIn']) {
 					</div>
 				</div>
 		</section>
-
 	</section>
-	<footer>
-				<a href="http://validator.w3.org/check?uri=referer">Valid HTML 5</a>
-				<a href="http://jigsaw.w3.org/css-validator/check/referer">
-					<img style="border:0;width:88px;height:31px"
-						src="http://jigsaw.w3.org/css-validator/images/vcss"
-						alt="Valid CSS!" />
-				</a>
-			</footer>
+	<footer><!--Contains some information that may be of interest to a small portion of users. Likely to missed by screen readers as it's the final piece of content on every page.-->
+		<a href="http://validator.w3.org/check?uri=referer">Valid HTML 5</a>
+		<a href="http://jigsaw.w3.org/css-validator/check/referer">
+			<img style="border:0;width:88px;height:31px"
+				src="http://jigsaw.w3.org/css-validator/images/vcss"
+				alt="Valid CSS!" />
+		</a>
+	</footer>
 </body>
 </html>
