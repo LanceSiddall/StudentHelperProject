b8ff774906cbf0a8729b19ebe93dbc251230d9f2
AGrant324 <AGrant324@users.noreply.github.com>
2017-04-28 18:57:18 +0100

    Update lectAddRoom.php

--- studentHelper/lectAddRoom.php
+++ studentHelper/lectAddRoom.php
@@ -1,15 +1,14 @@
 <?PHP
+require 'Database/configure.php';//Allows functions from 'Database/configure.php' to be run
 
-require 'Database/configure.php';
+$conn=mysqli_connect(DB_SERVER, DB_USER, DB_PASS, MYSQL_DB);//Runs mysqli_connect with parameters intialised in configure.php with the result stored in $conn
 
-$conn=mysqli_connect(DB_SERVER, DB_USER, DB_PASS, MYSQL_DB);
+$buildName = $_POST['buildingName'];//Loads the form variable buildingName into the PHP variable buildName
+$roomName = $_POST['roomName'];//Loads the form variable roomName into the PHP variable roomName
+$maxStudents = $_POST['maxStudents'];//Loads the form variable maxStudents into the PHP variable maxStudents
+$projector = $_POST['projector'];//Loads the form variable projector into the PHP variable projector
 
-$buildName = $_POST['buildingName'];
-$roomName = $_POST['roomName'];
-$maxStudents = $_POST['maxStudents'];
-$projector = $_POST['projector'];
-
-$newRoom = $conn->query("INSERT INTO room (buildingName, roomName, maxStudents, projector) VALUES ('{$buildName}','{$roomName}','{$maxStudents}','{$projector}')");
+$newRoom = $conn->query("INSERT INTO room (buildingName, roomName, maxStudents, projector) VALUES ('{$buildName}','{$roomName}','{$maxStudents}','{$projector}')");//Inserts the new data into the SQL table of room
 
 header('location: addRoom.php');
 
