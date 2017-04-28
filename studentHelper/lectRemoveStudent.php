31f7b0f56b7faa0224558e246ae686111bf47bbb
AGrant324 <AGrant324@users.noreply.github.com>
2017-04-28 18:59:37 +0100

    Update lectRemoveStudent.php

--- studentHelper/lectRemoveStudent.php
+++ studentHelper/lectRemoveStudent.php
@@ -1,17 +1,16 @@
 <?php
-require 'Database/configure.php';
+require 'Database/configure.php';//Allows functions from 'Database/configure.php' to be run
 
-session_start();
-if(!$_SESSION['loggedIn']) {
+session_start();//Starts a php session so that if one hasn't already been started, there isn't an issue caused by calling a session variable
+if(!$_SESSION['loggedIn']) {//If the user attempts to access this page without being logged in, they will be redirected to the login page
 	header('location: index.php');
 }
 
 $conn=mysqli_connect(DB_SERVER, DB_USER, DB_PASS, MYSQL_DB);
-$email = $_POST['StudentEmail'];
+$email = $_POST['StudentEmail'];//Loads the form variable StudentEmail into the PHP variable email
 $result = $conn->query("DELETE FROM `users` WHERE `users`.`email` = '{$email}'");
 echo "<script src='javascript.js'>
-				alert('User Removed!')
+				alert('User Removed!')//Creates a JS alert informing the user that they've succesfully removed a user
 			<script>"
-header('location: removeStudent.php');
-
+header('location: removeStudent.php')//Returns the user to removeStudent
 ?>
